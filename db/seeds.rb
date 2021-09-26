0e2b8dc25d10156a75dab44d9c13bdd4b9f97734
igaryo0506 <igarashi-ryo0506@g.ecc.u-tokyo.ac.jp>
2021-09-26 09:58:59 +0000

    feat:seed値の追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 User.create(name: "test",password: "password", password_confirmation: "password")
 User.create(name: "test2",password: "password", password_confirmation: "password")
+Room.create(name: "2020spring", number: "111111")
+UserRoomRelationship.create(user_id: 1, room_id: 1)
